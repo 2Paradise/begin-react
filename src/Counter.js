c652595bf6451716d4f77b78d351c312db1686be
2Paradise <robinnw@naver.com>
2020-07-10 18:11:20 +0900

    begin react git update

--- src/Counter.js
+++ src/Counter.js
@@ -1,38 +1,80 @@
-import React, {useReducer} from "react";
+import React, {Component} from "react";
 
-function reducer(state, action){
-    switch (action.type) {
-        case 'INCREMENT' :
-            return state + 1;
-        case 'DECREMENT' :
-            return state - 1;
-        default:
-            throw new Error('Unhandled action');
+class Counter extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter : 0,
+            fixed : 1,
+            updateMe : {
+                toggleMe : false,
+                dontChangeMe : 1,
+            }
+        }
     }
-}
+    handleIncrease = () => {
+        console.log('increase');
+        // this.setState({
+        //     counter : this.state.counter + 1
+        // });
 
-function Counter (){
+        this.setState(state => ({
+            counter : this.state.counter + 1
+        }));
+    }
 
-    const [number, dispatch] = useReducer(reducer, 0);
-
-    const onIncrease = () => {
-        dispatch({
-            type : 'INCREMENT'
+    handleDecrease = () => {
+        console.log('decrease');
+        this.setState({
+            counter : this.state.counter - 1
         });
     }
-    const onDecrease = () =>{
-        dispatch({
-            type : 'DECREMENT'
-        });
-    }
 
-    return (
+    render() {
+        return (
         <div>
-            <h1>{number}</h1>
-            <button onClick={onIncrease} >+1</button>
-            <button onClick={onDecrease}>-1</button>
+            <h1>{this.state.counter}</h1>
+            <button onClick={this.handleIncrease} >+1</button>
+            <button onClick={this.handleDecrease}>-1</button>
+            <p>고정된 값 : {this.state.fixed}</p>
         </div>
-    )
+        );
+    }
 }
+
+// function reducer(state, action){
+//     switch (action.type) {
+//         case 'INCREMENT' :
+//             return state + 1;
+//         case 'DECREMENT' :
+//             return state - 1;
+//         default:
+//             throw new Error('Unhandled action');
+//     }
+// }
+//
+// function Counter (){
+//
+//     const [number, dispatch] = useReducer(reducer, 0);
+//
+//     const onIncrease = () => {
+//         dispatch({
+//             type : 'INCREMENT'
+//         });
+//     }
+//     const onDecrease = () =>{
+//         dispatch({
+//             type : 'DECREMENT'
+//         });
+//     }
+//
+//     return (
+//         <div>
+//             <h1>{number}</h1>
+//             <button onClick={onIncrease} >+1</button>
+//             <button onClick={onDecrease}>-1</button>
+//         </div>
+//     )
+// }
 
 export default Counter;
--- src/Hello.js
+++ src/Hello.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, {Component} from "react";
 import './App.css';
 
-function Hello({color, name, isSpecial}) {
-    return (
-        <>
+class Hello extends Component {
+    static defaultProps = {
+        name : '이름 없음'
+    };
+
+    render() {
+        const {color, isSpecial, name} = this.props;
+
+        return (
             <div style={{color}}>
-                {isSpecial && <b> * </b>}
+                {isSpecial && <b>*</b>}
                 안녕하세요. {name}
             </div>
-        </>
-    );
+        );
+    }
 }
 
-Hello.defaultProps = {
-    name : '이름없음'
-}
+// function Hello({color, name, isSpecial}) {
+//     return (
+//         <>
+//             <div style={{color}}>
+//                 {isSpecial && <b> * </b>}
+//                 안녕하세요. {name}
+//             </div>
+//         </>
+//     );
+// }
+//
+// Hello.defaultProps = {
+//     name : '이름없음'
+// }
 
 export default Hello;
