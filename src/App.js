f07a6dfe8dd5f0bb6e27ea7863bc3399bc30cc8a
2Paradise <robinnw@naver.com>
2020-07-15 19:40:44 +0900

    begin react git update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React, {useReducer, useMemo, createContext} from 'react';
+import React, {useReducer, useMemo, createContext, useState} from 'react';
 import produce from 'immer';
 import UserList from './UserList';
 import CreateUser from "./CreateUser";
+import CheckBox from "./components/CheckBox";
+import Button from "./Button";
+import './App.scss';
 
 function countActiveUser(users){
     return users.filter(user => user.active).length;
@@ -71,10 +74,15 @@ function App() {
         , padding : "50px"
     };
 
+    const [check, setCheck] = useState(false);
     const [state, dispatch] = useReducer(reducer, initialState);
     const {users} = state;
 
     const count = useMemo(() => countActiveUser(users), [users]);
+    const onChange = (e) =>{
+        setCheck(e.target.checked);
+    }
+
     return (
         <UserDispatch.Provider value={dispatch}>
             <div style={style}>
@@ -82,6 +90,37 @@ function App() {
                 <UserList users={users}/>
                 <br/>
                 <div>활성 사용자 수 : {count}</div>
+                <br/>
+                <div className="App">
+                    <div className="buttons">
+                        <Button color="blue" size="large">Button</Button>
+                        <Button color="blue">Button</Button>
+                        <Button color="blue" size="small">Button</Button>
+                    </div>
+                    <div className="buttons">
+                        <Button color="gray" size="large">Button</Button>
+                        <Button color="gray">Button</Button>
+                        <Button color="gray" size="small">Button</Button>
+                    </div>
+                    <div className="buttons">
+                        <Button color="pink" size="large">Button</Button>
+                        <Button color="pink">Button</Button>
+                        <Button color="pink" size="small">Button</Button>
+                    </div>
+                    <div className="buttons">
+                        <Button size="large" outline>Button</Button>
+                        <Button color="gray" outline>Button</Button>
+                        <Button color="pink" size="small" outline>Button</Button>
+                    </div>
+                    <div className="buttons">
+                        <Button size="large" fullWidth>Button</Button>
+                        <Button color="gray" fullWidth>Button</Button>
+                        <Button color="pink" size="small" fullWidth onClick={() => console.log('click')}>Button</Button>
+                    </div>
+                </div>
+
+                <CheckBox checked={check} onChange={onChange}>다음 약관에 모두 동의</CheckBox>
+
             </div>
         </UserDispatch.Provider>
     );
